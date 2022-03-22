cf1e7ff6d1ea13e229a1f1d4ee025a562abb1bfb
johndwatt <jdw23@protonmail.com>
2022-03-22 11:59:25 -0700

    initializes middleware for cors, env, json

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const env = require('dotenv');
 
 // Configuration
+env.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 // Connection string
 app.listen(PORT, () => {
