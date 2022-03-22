7b5645905a3ed43095dc5b339b86d1ed77cd1266
johndwatt <jdw23@protonmail.com>
2022-03-22 12:01:42 -0700

    adds logger middleware

---
+++ middleware/logger.js
@@ -0,0 +1,13 @@
+/**
+ * Logs each HTTP request to the server console to assist with debugging.
+ * @param {*} req Express request param.
+ * @param {*} res Express response param.
+ * @param {*} next Express next param.
+ * @returns {String} Console.log of [URL]: [HTTP Request] - [Time of Request].
+ */
+ const logger = function(req, res, next){
+    console.log(`${req.url}: ${req.method} - ${new Date().toLocaleTimeString()}`);
+    next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(require("./middleware/logger"));
 
 // Connection string
 app.listen(PORT, () => {
