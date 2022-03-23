6d1d0e9949b1b682ace9833701b5169600847a7b
johndwatt <jdw23@protonmail.com>
2022-03-22 18:15:22 -0700

    adds working random thought route to home ctrl

--- controllers/home_ctrl.js
+++ controllers/home_ctrl.js
@@ -1,7 +1,27 @@
 const { Thought } = require("../models");
 
-const randomThought = async () => {
-
+const randomThought = async (req, res, next) => {     
+    try {
+        const thoughts = await Thought.find();
+        const randomIndex = Math.floor(Math.random() * thoughts.length)
+        const randomThought = thoughts.splice(randomIndex, 1)
+        if (randomThought !== undefined) {
+            return res.status(200).json({
+                randomThought,
+                msg: "Random thought found!",
+            });
+        } else {
+            return res.status(200).json({
+                msg: "No shower thoughts were found. Please try again later.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            error: error,
+            msg: "Something went wrong. Please try again."
+        });
+    }
 }
 
 module.exports = {
--- routes/home_routes.js
+++ routes/home_routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controllers = require("../controllers");
 
 // Routes
-
+router.get("/random", controllers.home_ctrl.randomThought);
 
 module.exports = router;
